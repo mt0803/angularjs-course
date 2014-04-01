a5b4e90d4f4204d916dbe66ad23711a3d3edc674
tao.meng <tao.meng@dianping.com>
2014-04-01 20:08:54 +0800

    update code

--- app/js/app.js
+++ app/js/app.js
@@ -15,7 +15,7 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/studyRoute', {templateUrl: 'partials/studyRoute.html'});
   $routeProvider.when('/studyFilter', {templateUrl: 'partials/studyFilter.html'});
   $routeProvider.when('/studyExecute', {templateUrl: 'partials/studyExecute.html'});
-  $routeProvider.when('/view7', {templateUrl: 'partials/partial_scope.html', controller: 'TimeCtrl'});
+  $routeProvider.when('/studyScopeCommunication', {templateUrl: 'partials/studyScopeCommunication.html'});
   $routeProvider.when('/view8', {templateUrl: 'partials/partial_controllerExt.html', controller: 'MainCtrl'});
   $routeProvider.when('/view9', {templateUrl: 'partials/partial_select.html', controller: 'CheckboxCtrl'});
   $routeProvider.when('/angularTree',{templateUrl:'partials/angularTree.html',controller:'treeCtrl'});
