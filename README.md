f8169c238d6980585f7e9875c3bdb63235676cce
tao.meng <tao.meng@dianping.com>
2014-04-15 15:40:34 +0800

    clear pro

--- README.md
+++
@@ -1,9 +0,0 @@
-Angularjs Course By MT
-
-Get started:
-
-1. 安装nodejs  设置环境变量
-2. git clone 代码
-3. 进入angularjs-course目录，执行 node script/web-server.js
-4. 访问页面：http://localhost:8000/app/index.html
-5. OK ~~~ 开始探索吧~~~Go go go~~~
--- scripts/e2e-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo ""
-echo "Starting Karma Server (http://karma-runner.github.io)"
-echo $BASE_DIR
-echo "-------------------------------------------------------------------"
-
-karma start ../config/karma-e2e.conf.js $*
